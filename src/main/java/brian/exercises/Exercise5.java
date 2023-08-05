1a74676f97a0d0b640c59c8f1e3dfc13b0b0e147
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-08-05 22:05:47 +0100

    excersice 5 and test done

--- src/main/java/brian/exercises/Exercise5.java
+++ src/main/java/brian/exercises/Exercise5.java
@@ -1,5 +1,26 @@
 package brian.exercises;
 
+import java.util.Scanner;
+
 public class Exercise5 {
-    
+    public static boolean isPerfectNumber(int number) {
+        int sumOfDivisors = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            sumOfDivisors += (number % i == 0) ? i : 0;
+        }
+
+        return sumOfDivisors == number;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce un numero: ");
+        int number = sc.nextInt();
+
+        System.out.println(number +  (isPerfectNumber(number) ? " es " : " no es ") + "un numero perfecto.");
+
+        sc.close();
+    }
 }
--- src/test/java/brian/exercises/Exercise5Test.java
+++ src/test/java/brian/exercises/Exercise5Test.java
@@ -1,5 +1,20 @@
 package brian.exercises;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class Exercise5Test {
-    
+    @Test
+    public void testIsPerfectNumber() {
+
+        assertEquals(true, Exercise5.isPerfectNumber(6));
+        assertEquals(true, Exercise5.isPerfectNumber(28));
+        assertEquals(true, Exercise5.isPerfectNumber(496));
+        assertEquals(true, Exercise5.isPerfectNumber(8128));
+
+        assertEquals(false, Exercise5.isPerfectNumber(7));
+        assertEquals(false, Exercise5.isPerfectNumber(12));
+        assertEquals(false, Exercise5.isPerfectNumber(91));
+        assertEquals(false, Exercise5.isPerfectNumber(12345));
+    }
 }
