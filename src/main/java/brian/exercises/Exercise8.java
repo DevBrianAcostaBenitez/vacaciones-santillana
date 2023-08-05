f553dbb8fc8114e6078058e4b5dd4d198cf688f7
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-08-05 23:04:22 +0100

    exercise 8 and test done

--- src/main/java/brian/exercises/Exercise8.java
+++ src/main/java/brian/exercises/Exercise8.java
@@ -1,5 +1,38 @@
 package brian.exercises;
 
+import java.util.Scanner;
+
 public class Exercise8 {
-    
+
+    public static int sumOfDivisors(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static boolean areAmicableNumbers(int number1, int number2) {
+        return sumOfDivisors(number1) == number2 && sumOfDivisors(number2) == number1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el primer numero: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Introduce el segundo numero: ");
+        int number2 = sc.nextInt();
+
+        if (areAmicableNumbers(number1, number2)) {
+            System.out.println(number1 + " y " + number2 + " son numeros amigos.");
+        } else {
+            System.out.println(number1 + " y " + number2 + " son numeros amigos.");
+        }
+
+        sc.close();
+    }
 }
--- src/test/java/brian/exercises/Exercise8Test.java
+++ src/test/java/brian/exercises/Exercise8Test.java
@@ -1,5 +1,21 @@
 package brian.exercises;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+
 public class Exercise8Test {
-    
+
+    @Test
+    public void testAreAmicableNumbers() {
+        assertTrue(Exercise8.areAmicableNumbers(220, 284));
+        assertTrue(Exercise8.areAmicableNumbers(1184, 1210));
+        assertTrue(Exercise8.areAmicableNumbers(2620, 2924));
+        assertTrue(Exercise8.areAmicableNumbers(5020, 5564));
+        
+        assertFalse(Exercise8.areAmicableNumbers(220, 221));
+        assertFalse(Exercise8.areAmicableNumbers(1184, 1211));
+        assertFalse(Exercise8.areAmicableNumbers(2620, 2925));
+        assertFalse(Exercise8.areAmicableNumbers(5020, 5565));
+    }
 }
